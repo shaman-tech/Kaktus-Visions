0a58d094431bcf6bf1e853525198a1a5420ffcc3
Peter Gordon <petergordon@Peters-MBP-2.home>
2017-03-23 00:26:02 -0400

    Pizza party started

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt){
         dest: 'scripts/scripts.js',
       },
       css: {
-        src: ['css/main.css',],
+        src: ['css/output.css',],
         dest: 'scripts/styles.css',
       },
     },
--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,38 @@
 /* collects and stores signups */
+
+
+var people = {
+
+    people: [],
+
+    init: function () {
+          this.cacheDom();
+          this.bindEvents();
+          //this.render();
+
+    },
+    cacheDom: function() {
+        this.$el = $('#container');
+        this.$input = this.$el.find('input');
+        this.$button = this.$el.find('button');
+
+    },
+
+    bindEvents: function(){
+
+        this.$button.on("click", this.addPerson.bind(this));
+    },
+    render: function(){
+
+            //print results
+
+    },
+    addPerson: function(){
+
+            console.log('hello');
+
+    }
+
+  };
+
+  people.init();
